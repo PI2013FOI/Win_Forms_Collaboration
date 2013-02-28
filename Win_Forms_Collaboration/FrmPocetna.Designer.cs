36f113701a643ded03da3f5b1e52dfd89d9d36d9
Marko M <mijac.marko@gmail.com>
2013-02-28 13:00:11 +0100

    Inicijalni commit
    Složena forma za gumbima za svakog studenta.

---
+++ Win_Forms_Collaboration/FrmPocetna.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Win_Forms_Collaboration
+{
+    partial class FrmPocetna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPocetna));
+            this.btnStudent1 = new System.Windows.Forms.Button();
+            this.btnStudent2 = new System.Windows.Forms.Button();
+            this.btnStudent3 = new System.Windows.Forms.Button();
+            this.btnStudent4 = new System.Windows.Forms.Button();
+            this.btnStudent05 = new System.Windows.Forms.Button();
+            this.btnStudent6 = new System.Windows.Forms.Button();
+            this.btnStudent7 = new System.Windows.Forms.Button();
+            this.btnStudent8 = new System.Windows.Forms.Button();
+            this.btnStudent9 = new System.Windows.Forms.Button();
+            this.btnStudent10 = new System.Windows.Forms.Button();
+            this.btnStudent11 = new System.Windows.Forms.Button();
+            this.btnStudent12 = new System.Windows.Forms.Button();
+            this.btnStudent13 = new System.Windows.Forms.Button();
+            this.btnStudent14 = new System.Windows.Forms.Button();
+            this.btnStudent15 = new System.Windows.Forms.Button();
+            this.btnStudent16 = new System.Windows.Forms.Button();
+            this.btnStudent17 = new System.Windows.Forms.Button();
+            this.btnStudent18 = new System.Windows.Forms.Button();
+            this.btnStudent19 = new System.Windows.Forms.Button();
+            this.btnStudent20 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStudent1
+            // 
+            this.btnStudent1.Location = new System.Drawing.Point(106, 73);
+            this.btnStudent1.Name = "btnStudent1";
+            this.btnStudent1.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent1.TabIndex = 0;
+            this.btnStudent1.Text = "Student 01";
+            this.btnStudent1.UseVisualStyleBackColor = true;
+            this.btnStudent1.Click += new System.EventHandler(this.btnStudent1_Click);
+            // 
+            // btnStudent2
+            // 
+            this.btnStudent2.Location = new System.Drawing.Point(106, 102);
+            this.btnStudent2.Name = "btnStudent2";
+            this.btnStudent2.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent2.TabIndex = 1;
+            this.btnStudent2.Text = "Student 02";
+            this.btnStudent2.UseVisualStyleBackColor = true;
+            this.btnStudent2.Click += new System.EventHandler(this.btnStudent2_Click);
+            // 
+            // btnStudent3
+            // 
+            this.btnStudent3.Location = new System.Drawing.Point(106, 131);
+            this.btnStudent3.Name = "btnStudent3";
+            this.btnStudent3.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent3.TabIndex = 2;
+            this.btnStudent3.Text = "Student 03";
+            this.btnStudent3.UseVisualStyleBackColor = true;
+            this.btnStudent3.Click += new System.EventHandler(this.btnStudent3_Click);
+            // 
+            // btnStudent4
+            // 
+            this.btnStudent4.Location = new System.Drawing.Point(106, 160);
+            this.btnStudent4.Name = "btnStudent4";
+            this.btnStudent4.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent4.TabIndex = 3;
+            this.btnStudent4.Text = "Student 04";
+            this.btnStudent4.UseVisualStyleBackColor = true;
+            this.btnStudent4.Click += new System.EventHandler(this.btnStudent4_Click);
+            // 
+            // btnStudent05
+            // 
+            this.btnStudent05.Location = new System.Drawing.Point(106, 189);
+            this.btnStudent05.Name = "btnStudent05";
+            this.btnStudent05.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent05.TabIndex = 4;
+            this.btnStudent05.Text = "Student 05";
+            this.btnStudent05.UseVisualStyleBackColor = true;
+            this.btnStudent05.Click += new System.EventHandler(this.btnStudent05_Click);
+            // 
+            // btnStudent6
+            // 
+            this.btnStudent6.Location = new System.Drawing.Point(106, 218);
+            this.btnStudent6.Name = "btnStudent6";
+            this.btnStudent6.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent6.TabIndex = 5;
+            this.btnStudent6.Text = "Student 06";
+            this.btnStudent6.UseVisualStyleBackColor = true;
+            this.btnStudent6.Click += new System.EventHandler(this.btnStudent6_Click);
+            // 
+            // btnStudent7
+            // 
+            this.btnStudent7.Location = new System.Drawing.Point(106, 247);
+            this.btnStudent7.Name = "btnStudent7";
+            this.btnStudent7.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent7.TabIndex = 6;
+            this.btnStudent7.Text = "Student 07";
+            this.btnStudent7.UseVisualStyleBackColor = true;
+            this.btnStudent7.Click += new System.EventHandler(this.btnStudent7_Click);
+            // 
+            // btnStudent8
+            // 
+            this.btnStudent8.Location = new System.Drawing.Point(106, 276);
+            this.btnStudent8.Name = "btnStudent8";
+            this.btnStudent8.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent8.TabIndex = 7;
+            this.btnStudent8.Text = "Student 08";
+            this.btnStudent8.UseVisualStyleBackColor = true;
+            this.btnStudent8.Click += new System.EventHandler(this.btnStudent8_Click);
+            // 
+            // btnStudent9
+            // 
+            this.btnStudent9.Location = new System.Drawing.Point(106, 304);
+            this.btnStudent9.Name = "btnStudent9";
+            this.btnStudent9.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent9.TabIndex = 8;
+            this.btnStudent9.Text = "Student 9";
+            this.btnStudent9.UseVisualStyleBackColor = true;
+            this.btnStudent9.Click += new System.EventHandler(this.btnStudent9_Click);
+            // 
+            // btnStudent10
+            // 
+            this.btnStudent10.Location = new System.Drawing.Point(106, 333);
+            this.btnStudent10.Name = "btnStudent10";
+            this.btnStudent10.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent10.TabIndex = 9;
+            this.btnStudent10.Text = "Student 10";
+            this.btnStudent10.UseVisualStyleBackColor = true;
+            this.btnStudent10.Click += new System.EventHandler(this.btnStudent10_Click);
+            // 
+            // btnStudent11
+            // 
+            this.btnStudent11.Location = new System.Drawing.Point(262, 73);
+            this.btnStudent11.Name = "btnStudent11";
+            this.btnStudent11.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent11.TabIndex = 10;
+            this.btnStudent11.Text = "Student 11";
+            this.btnStudent11.UseVisualStyleBackColor = true;
+            this.btnStudent11.Click += new System.EventHandler(this.btnStudent11_Click);
+            // 
+            // btnStudent12
+            // 
+            this.btnStudent12.Location = new System.Drawing.Point(262, 102);
+            this.btnStudent12.Name = "btnStudent12";
+            this.btnStudent12.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent12.TabIndex = 11;
+            this.btnStudent12.Text = "Student 12";
+            this.btnStudent12.UseVisualStyleBackColor = true;
+            this.btnStudent12.Click += new System.EventHandler(this.btnStudent12_Click);
+            // 
+            // btnStudent13
+            // 
+            this.btnStudent13.Location = new System.Drawing.Point(262, 131);
+            this.btnStudent13.Name = "btnStudent13";
+            this.btnStudent13.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent13.TabIndex = 12;
+            this.btnStudent13.Text = "Student 13";
+            this.btnStudent13.UseVisualStyleBackColor = true;
+            this.btnStudent13.Click += new System.EventHandler(this.btnStudent13_Click);
+            // 
+            // btnStudent14
+            // 
+            this.btnStudent14.Location = new System.Drawing.Point(262, 160);
+            this.btnStudent14.Name = "btnStudent14";
+            this.btnStudent14.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent14.TabIndex = 13;
+            this.btnStudent14.Text = "Student 14";
+            this.btnStudent14.UseVisualStyleBackColor = true;
+            this.btnStudent14.Click += new System.EventHandler(this.btnStudent14_Click);
+            // 
+            // btnStudent15
+            // 
+            this.btnStudent15.Location = new System.Drawing.Point(262, 189);
+            this.btnStudent15.Name = "btnStudent15";
+            this.btnStudent15.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent15.TabIndex = 14;
+            this.btnStudent15.Text = "Student 15";
+            this.btnStudent15.UseVisualStyleBackColor = true;
+            this.btnStudent15.Click += new System.EventHandler(this.btnStudent15_Click);
+            // 
+            // btnStudent16
+            // 
+            this.btnStudent16.Location = new System.Drawing.Point(262, 218);
+            this.btnStudent16.Name = "btnStudent16";
+            this.btnStudent16.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent16.TabIndex = 15;
+            this.btnStudent16.Text = "Student 16";
+            this.btnStudent16.UseVisualStyleBackColor = true;
+            this.btnStudent16.Click += new System.EventHandler(this.btnStudent16_Click);
+            // 
+            // btnStudent17
+            // 
+            this.btnStudent17.Location = new System.Drawing.Point(262, 247);
+            this.btnStudent17.Name = "btnStudent17";
+            this.btnStudent17.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent17.TabIndex = 16;
+            this.btnStudent17.Text = "Student 17";
+            this.btnStudent17.UseVisualStyleBackColor = true;
+            this.btnStudent17.Click += new System.EventHandler(this.btnStudent17_Click);
+            // 
+            // btnStudent18
+            // 
+            this.btnStudent18.Location = new System.Drawing.Point(262, 276);
+            this.btnStudent18.Name = "btnStudent18";
+            this.btnStudent18.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent18.TabIndex = 17;
+            this.btnStudent18.Text = "Student 18";
+            this.btnStudent18.UseVisualStyleBackColor = true;
+            this.btnStudent18.Click += new System.EventHandler(this.btnStudent18_Click);
+            // 
+            // btnStudent19
+            // 
+            this.btnStudent19.Location = new System.Drawing.Point(262, 304);
+            this.btnStudent19.Name = "btnStudent19";
+            this.btnStudent19.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent19.TabIndex = 18;
+            this.btnStudent19.Text = "Student 19";
+            this.btnStudent19.UseVisualStyleBackColor = true;
+            this.btnStudent19.Click += new System.EventHandler(this.btnStudent19_Click);
+            // 
+            // btnStudent20
+            // 
+            this.btnStudent20.Location = new System.Drawing.Point(262, 333);
+            this.btnStudent20.Name = "btnStudent20";
+            this.btnStudent20.Size = new System.Drawing.Size(150, 23);
+            this.btnStudent20.TabIndex = 19;
+            this.btnStudent20.Text = "Student 20";
+            this.btnStudent20.UseVisualStyleBackColor = true;
+            this.btnStudent20.Click += new System.EventHandler(this.btnStudent20_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(513, 46);
+            this.label1.TabIndex = 20;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // FrmPocetna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 378);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStudent20);
+            this.Controls.Add(this.btnStudent19);
+            this.Controls.Add(this.btnStudent18);
+            this.Controls.Add(this.btnStudent17);
+            this.Controls.Add(this.btnStudent16);
+            this.Controls.Add(this.btnStudent15);
+            this.Controls.Add(this.btnStudent14);
+            this.Controls.Add(this.btnStudent13);
+            this.Controls.Add(this.btnStudent12);
+            this.Controls.Add(this.btnStudent11);
+            this.Controls.Add(this.btnStudent10);
+            this.Controls.Add(this.btnStudent9);
+            this.Controls.Add(this.btnStudent8);
+            this.Controls.Add(this.btnStudent7);
+            this.Controls.Add(this.btnStudent6);
+            this.Controls.Add(this.btnStudent05);
+            this.Controls.Add(this.btnStudent4);
+            this.Controls.Add(this.btnStudent3);
+            this.Controls.Add(this.btnStudent2);
+            this.Controls.Add(this.btnStudent1);
+            this.Name = "FrmPocetna";
+            this.Text = "Početna forma aplikacije";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStudent1;
+        private System.Windows.Forms.Button btnStudent2;
+        private System.Windows.Forms.Button btnStudent3;
+        private System.Windows.Forms.Button btnStudent4;
+        private System.Windows.Forms.Button btnStudent05;
+        private System.Windows.Forms.Button btnStudent6;
+        private System.Windows.Forms.Button btnStudent7;
+        private System.Windows.Forms.Button btnStudent8;
+        private System.Windows.Forms.Button btnStudent9;
+        private System.Windows.Forms.Button btnStudent10;
+        private System.Windows.Forms.Button btnStudent11;
+        private System.Windows.Forms.Button btnStudent12;
+        private System.Windows.Forms.Button btnStudent13;
+        private System.Windows.Forms.Button btnStudent14;
+        private System.Windows.Forms.Button btnStudent15;
+        private System.Windows.Forms.Button btnStudent16;
+        private System.Windows.Forms.Button btnStudent17;
+        private System.Windows.Forms.Button btnStudent18;
+        private System.Windows.Forms.Button btnStudent19;
+        private System.Windows.Forms.Button btnStudent20;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Win_Forms_Collaboration/FrmPocetna.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Forms_Collaboration
+{
+    public partial class FrmPocetna : Form
+    {
+        public FrmPocetna()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStudent1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent05_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStudent20_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
